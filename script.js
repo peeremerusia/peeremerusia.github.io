604820fabc1091f595aa67582c41a674252f7041
peeremerusia <tenpeerawattt@gmail.com>
2026-08-27 13:35:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded',function () {
+  document.getElementById('year').innerHTML = new DATA().getFullYear();
   console.log('Portfolio ของ PEERAMERUSIA พร้อมแล้ว!');
 });
